f7a04b19d9e9c76344a228e3d734a79a8e0652ce
FrederikOE <fkoefkoe@live.dk>
2023-09-06 21:13:46 +0200

    Add the non micro frontend version

---
+++ apps/pokeDuel/README.md
@@ -0,0 +1,59 @@
+# Poké duel - event management
+
+A development exercise.
+
+Focus:
+
+- Deliver PoC which adequately demonstrates potential of application
+- Develop within a limited timeframe (tools and architecture decisions based on this)
+- Demonstrate technical knowhow
+
+Features:
+
+- List all upcoming duels
+- Login as admin or normal user
+- Edit duel (admin only)
+- Create duel (admin only)
+
+Tools:
+
+- Typescript
+- Poké API https://pokeapi.co/
+- Chakra UI https://chakra-ui.com/
+- Tailwind https://tailwindcss.com/
+- Create React App https://create-react-app.dev/
+
+Mocked functionality:
+
+- Match API
+  - getMatches endpoint in the mocked API
+  - update and create mocked in logic layer
+- Authentication
+  - No API layer, mocked in logic layer
+
+Not included:
+
+- Selecting time of day when creating or editing duels (always uses 02:00)
+- Persisting state, login as well as duel state
+- Logout feature (refresh works)
+- Comprehensive form validation
+- Accessibility (for purposes of PoC not in focus)
+- Testing
+- Comprehensive documentation (though parts are well documented)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
---
+++ apps/pokeDuel/src/apis/pokemonAPI.types.ts
@@ -0,0 +1,132 @@
+export interface IPokemonAbility {
+  is_hidden: boolean;
+  slot: number;
+  ability: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface IPokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface IPokemonFormType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface IPokemonTypePast {
+  generation: {
+    name: string;
+    url: string;
+  };
+  types: IPokemonType[];
+}
+
+export interface IPokemonHeldItem {
+  item: {
+    name: string;
+    url: string;
+  };
+  version_details: {
+    version: {
+      name: string;
+      url: string;
+    };
+    rarity: number;
+  }[];
+}
+
+export interface IPokemonMove {
+  move: {
+    name: string;
+    url: string;
+  };
+  version_group_details: {
+    move_learn_method: {
+      name: string;
+      url: string;
+    };
+    version_group: {
+      name: string;
+      url: string;
+    };
+    level_learned_at: number;
+  }[];
+}
+
+export interface IPokemonStat {
+  stat: {
+    name: string;
+    url: string;
+  };
+  effort: number;
+  base_stat: number;
+}
+
+export interface IPokemonSprites {
+  front_default: string;
+  front_shiny: string;
+  front_female: string;
+  front_shiny_female: string;
+  back_default: string;
+  back_shiny: string;
+  back_female: string;
+  back_shiny_female: string;
+  other: {
+    dream_world: {
+      front_default: string;
+    };
+  };
+}
+
+export interface IPokemon {
+  id: number;
+  name: string;
+  base_experience: number;
+  height: number;
+  is_default: boolean;
+  order: number;
+  weight: number;
+  abilities: IPokemonAbility[];
+  forms: {
+    name: string;
+    url: string;
+  }[];
+  game_indices: {
+    game_index: number;
+    version: {
+      name: string;
+      url: string;
+    };
+  }[];
+  held_items: IPokemonHeldItem[];
+  location_area_encounters: string;
+  moves: IPokemonMove[];
+  past_types: IPokemonTypePast[];
+  sprites: IPokemonSprites;
+  species: {
+    name: string;
+    url: string;
+  };
+  stats: IPokemonStat[];
+  types: IPokemonType[];
+}
+
+export interface ILocation {
+  name: string;
+  url: string;
+}
+
+export interface IPokemonLookup {
+  name: string;
+  url: string;
+}
